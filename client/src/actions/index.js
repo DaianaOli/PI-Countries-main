484062480d21613010630081370d74fa50747f22
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-11-09 16:10:24 -0300

    .

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,7 +16,7 @@ import {
 export function getCountries() {
     return async (dispatch) => {
         try{
-        const res = await axios.get("http://localhost:3001/countries/");
+        const res = await axios.get("http://localhost:3001/countries");
         dispatch({ type: GET_COUNTRIES, payload: res.data });
         } catch (error) {
         console.log(error);
